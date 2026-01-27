20ff1a3331fcaa418f119b7a72899a73d8801a7b
Thiago Leal <thiagoleal238@gmail.com>
2026-01-27 17:22:19 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 a 10';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let NumeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -21,11 +21,11 @@ if('speechSynthesis' in window) {
   }
 }
 exibirTextoNaTela('h1', 'Jogo do numero secreto');
- exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+ exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do numero secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 exibirMensagemInicial();
 
